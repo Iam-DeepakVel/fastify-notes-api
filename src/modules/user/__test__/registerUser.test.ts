8e1bb6fbb2c035ff3230e34970a08772f426e322
Iam-DeepakVel <deepak.vel82@gmail.com>
2023-11-04 18:02:39 +0530

    feat: test-case update

--- src/modules/user/__test__/registerUser.test.ts
+++ src/modules/user/__test__/registerUser.test.ts
@@ -1,9 +1,9 @@
-import { faker } from '@faker-js/faker';
-import { test } from 'tap';
-import buildServer from '../../../server';
-import { connectDB, disconnectDB } from '../../../db/db';
+import { faker } from "@faker-js/faker";
+import { test } from "tap";
+import buildServer from "../../../server";
+import { connectDB, disconnectDB } from "../../../db/db";
 
-test('POST `api/users/register` route - Create User Successfull', async (t) => {
+test("POST `api/users/register` route - Create User Successfull", async (t) => {
   const name = faker.person.firstName();
   const email = faker.internet.email();
   const password = faker.internet.password();
@@ -20,13 +20,13 @@ test('POST `api/users/register` route - Create User Successfull', async (t) => {
 
   // Register user
   const response = await fastify.inject({
-    method: 'POST',
-    url: '/api/users/register',
+    method: "POST",
+    url: "/api/users/register",
     payload,
   });
 
   t.equal(response.statusCode, 201);
-  t.equal(response.headers['content-type'], 'application/json; charset=utf-8');
-  t.same(response.json().message, 'Registration Successful');
-  t.type(response.json().accessToken, 'string');
+  t.equal(response.headers["content-type"], "application/json; charset=utf-8");
+  t.same(response.json().message, "Registration Successful");
+  t.type(response.json().accessToken, "string");
 });
